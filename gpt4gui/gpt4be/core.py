1e7aaed601a6567d2ec3746ffba9450ee4222e0c
francescodipietr0 <francesco.dipietro0911@gmail.com>
2024-04-09 15:06:37 +0200

    add git ignore + fe and be linked

--- gpt4gui/gpt4be/core.py
+++ gpt4gui/gpt4be/core.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/api/test', methods=['POST'])
+@app.route('/api/test', methods=['GET'])
 def test_request():
-    return 'success!'
+    prompt = request.args.get('prompt')
+    result = 'success! il prompt era: ' + prompt
+
+    # chiamata a gpt 4 dando come parametro il promp ricevuto
+    # ritornare la risposta dell'api invocata
+    
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
---
+++ gpt4gui/gpt4gui/src/app/services/gpt.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GptService {
+
+  constructor(private http: HttpClient) { }
+
+  invokeGpt(prompt: string) {
+    let url = "http://localhost:8080/api/test?prompt=" + prompt;
+    this.http.get(url)
+      .subscribe(x => {
+        console.log(x);
+      })
+  }
+  
+}
